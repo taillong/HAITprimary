8b9ab3cc4fd947f318416489b867e4800f14f413
taillong <63985597+taillong@users.noreply.github.com>
2020-09-13 16:31:57 +0900

    モデル途中、url設定途中、widget-tweaksを消した。

--- HAITprimary/settings.py
+++ HAITprimary/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boostrap4',
-    'widget-tweaks',
+    'bootstrap4',
     'sashimi',
 ]
 
--- sashimi/models.py
+++ sashimi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):   
+    class Meta:
+        #テーブル名の指定
+        db_table ="category"
+
+    #カラム名の定義
+    category_name = models.CharField(max_length=255,unique=True)
+    content = models.CharField(max_length=1000,unique=True)
+    
+class Post(models.Model):
+    class Meta:
+        db_table = 'post'
+  
+    # カラム名
+    image =
+    category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
+    predict = 
