58468b52d74b1d2c7f6aff6228fa4ae4d5a53baa
taillong <63985597+taillong@users.noreply.github.com>
2020-09-14 01:27:41 +0900

    pillowをimportしただけ

--- sashimi/views.py
+++ sashimi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from PIL import Image
 
 def index(request):
     if request.method == "GET":
@@ -11,7 +11,16 @@ def index(request):
 
 def result(request):
     file = request.POST["file"]
-    
+
+    # 必要になるであろう処理
+    # image = Image.open(file)
+    # image = image.convert("RGB")
+    # image = image.resize((100, 100))
+    # data = np.asarray(image)
+    # X = []
+    # X.append(data)
+    # X = np.array(X)
+
 
     return render(
         request,
