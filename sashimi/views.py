4d8d99f70b6dc34cd2350d20b9ea1b141c8cdacf
taillong <63985597+taillong@users.noreply.github.com>
2020-09-14 18:18:57 +0900

    minor change

--- sashimi/views.py
+++ sashimi/views.py
@@ -5,6 +5,6 @@ from .models import Post
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         post_data = Post.objects.order_by('-id')
-        return render(request, 'sashimi/index.html'{
+        return render(request, 'sashimi/index.html',{
             'post_data':post_data
         })
