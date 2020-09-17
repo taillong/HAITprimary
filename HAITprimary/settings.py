e850a3d3770f869fd096f5f507fe5ce30497652e
檜原陽一郎 <hibarayoichiro@hibaruyouichirounoMacBook-puro.local>
2020-09-17 13:36:06 +0900

    Hibara

--- HAITprimary/settings.py
+++ HAITprimary/settings.py
@@ -65,6 +65,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins':[ 
+               'bootstrap4.templatetags.bootstrap4',
+            ],    
         },
     },
 ]
